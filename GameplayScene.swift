030643367ede5d9e3b505e5a1919f6e29e703825
Caroline Davis <caroline_davis@live.com>
2017-02-27 11:23:10 +1100

    feat: Add pause panel

--- GameplayScene.swift
+++ GameplayScene.swift
@@ -32,6 +32,9 @@ class GameplayScene: SKScene {
     let minX = CGFloat(-150)
     let maxX = CGFloat(150)
     
+    
+    private var pausePanel: SKSpriteNode?
+    
     override func didMove(to view: SKView) {
       initializeVariables()
     }
@@ -49,12 +52,34 @@ class GameplayScene: SKScene {
         for touch in touches {
             let location = touch.location(in: self)
             
+            if self.scene?.isPaused == false {
             if location.x > center! {
                 moveLeft = false
                 player?.animatePlayer(moveLeft: moveLeft)
             } else {
                 moveLeft = true
                 player?.animatePlayer(moveLeft: moveLeft)
+                }
+            }
+            
+            if atPoint(location).name == "Pause" {
+                self.scene?.isPaused = true
+                createPausePanel()
+            }
+            
+            if atPoint(location).name == "Resume" {
+                pausePanel?.removeFromParent()
+                self.scene?.isPaused = false
+                
+            }
+            
+            if atPoint(location).name == "Quit" {
+                let scene = MainMenuScene(fileNamed: "MainMenu")
+                // Set the scale mode to scale to fit the window
+                scene?.scaleMode = .aspectFill
+                
+                // loads the scene similiar to segue
+                self.view?.presentScene(scene!, transition: SKTransition.doorsCloseVertical(withDuration: 1))
             }
         }
         canMove = true
@@ -118,6 +143,44 @@ class GameplayScene: SKScene {
         }
     }
     
+    
+    func createPausePanel(){
+        
+        pausePanel = SKSpriteNode(imageNamed: "Pause Menu")
+        let resumeButton = SKSpriteNode(imageNamed: "Resume Button")
+        let quitButton = SKSpriteNode(imageNamed: "Quit Button 2")
+        
+        
+        pausePanel?.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        pausePanel?.xScale = 1.6
+        pausePanel?.yScale = 1.6
+        pausePanel?.zPosition = 4
+        
+        // sets the position of the pause panel - the panel will be the child of the camera and needs to be in the middle of the screen where the camera is.
+        pausePanel?.position = CGPoint(x: (self.mainCamera?.frame.size.width)! / 2, y: (self.mainCamera?.frame.height)! / 2)
+        
+        resumeButton.name = "Resume"
+        resumeButton.zPosition = 5
+        resumeButton.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        // sets the resume button into the pause panel correctly
+        resumeButton.position = CGPoint(x:(pausePanel?.position.x)!, y: (pausePanel?.position.y)! + 25)
+        
+        
+        quitButton.name = "Quit"
+        quitButton.zPosition = 5
+        quitButton.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        // sets the resume button into the pause panel correctly
+        quitButton.position = CGPoint(x:(pausePanel?.position.x)!, y: (pausePanel?.position.y)! - 45)
+        
+        // to show everything
+        
+        pausePanel?.addChild(resumeButton)
+        pausePanel?.addChild(quitButton)
+        
+        self.mainCamera?.addChild(pausePanel!)
+        
+        
+    }
     
 }
 
